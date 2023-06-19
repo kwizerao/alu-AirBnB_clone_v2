111d476e2f9007030aea41208c02242b3ee5e8b0
Ardinenukuri <a.nukuri@alustudent.com>
2023-06-19 13:20:12 +0200

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
-# describe it then
-apt-get update
-apt-get -y install nginx
+# Install nginx if not already installed
 
-directories=("/data/web_static/releases/test" "/data/web_static/shared/")
-
-for directory in "${directories[@]}"; do
-  #  if [ ! -e "$directory" ]; then
-  mkdir -p "$directory"
-  #  fi
-done
-
-echo "<html>
+if ! [ -x "$(command -v nginx)" ]; then
+  sudo apt-get update
+  sudo apt-get -y install nginx
+fi
+sudo service nginx start
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >/data/web_static/releases/test/index.html
-
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
-# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
-ln --symbolic --force /data/web_static/releases/test /data/web_static/current
-
-# The -R option ensures that the ownership changes are applied
-# recursively to all files and directories within the folder
-chown -R ubuntu:ubuntu /data/
-
-sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
-
-service nginx restart
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,77 +1,41 @@
 #!/usr/bin/python3
-"""Comment"""
-from fabric.api import *
+"""
+script that distributes an archive to my web servers,
+using the function (do_deploy:)
+"""
 import os
-import re
-from datetime import datetime
-
-env.user = 'ubuntu'
-env.hosts = ['3.80.74.138', '3.88.68.105']
-
-
-def do_pack():
-    """Comm"""
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
-        return None
-    return result
+from fabric.api import put, run, env
+env.hosts = ['34.224.26.174', '18.208.221.76']
 
 
 def do_deploy(archive_path):
-    """Comment"""
-    if not os.path.isfile(archive_path):
+    """
+    Distribution to my servers
+    """
+    if archive_path is None or not os.path.exists(archive_path):
         return False
-
-    filename_regex = re.compile(r'[^/]+(?=\.tgz$)')
-    match = filename_regex.search(archive_path)
-
-    # Upload the archive to the /tmp/ directory of the web server
-    archive_filename = match.group(0)
-    result = put(archive_path, "/tmp/{}.tgz".format(archive_filename))
-    if result.failed:
+    try:
+        file = archive_path.split("/")[-1]
+        file_name = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        # upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # create a folder with the same name as the archive
+        # without the extension
+        run("mkdir -p {}{}/".format(path, file_name))
+        # uncompress the archive to the folder
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            file, path, file_name))
+        # delete the archive from the web server
+        run("rm /tmp/{}".format(file))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_name))
+        # delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf {}{}/web_static".format(path, file_name))
+        run("rm -rf /data/web_static/current")
+        # create new symbolic link /data/web_static/current on web server
+        # linked to the new version of your code
+        run("ln -s {}{}/ /data/web_static/current".
+            format(path, file_name))
+        return True
+    except BaseException:
         return False
-    # Uncompress the archive to the folder
-    #     /data/web_static/releases/<archive filename without extension> on
-    #     the web server
-
-    result = run(
-        "mkdir -p /data/web_static/releases/{}/".format(archive_filename))
-    if result.failed:
-        return False
-    result = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-                 .format(archive_filename, archive_filename))
-    if result.failed:
-        return False
-
-    # Delete the archive from the web server
-    result = run("rm /tmp/{}.tgz".format(archive_filename))
-    if result.failed:
-        return False
-    result = run("mv /data/web_static/releases/{}"
-                 "/web_static/* /data/web_static/releases/{}/"
-                 .format(archive_filename, archive_filename))
-    if result.failed:
-        return False
-    result = run("rm -rf /data/web_static/releases/{}/web_static"
-                 .format(archive_filename))
-    if result.failed:
-        return False
-
-    # Delete the symbolic link /data/web_static/current from the web server
-    result = run("rm -rf /data/web_static/current")
-    if result.failed:
-        return False
-
-    #  Create a new the symbolic link
-    #  /data/web_static/current on the web server,
-    #     linked to the new version of your code
-    #     (/data/web_static/releases/<archive filename without extension>)
-    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                 .format(archive_filename))
-    if result.failed:
-        return False
-
-    return True
