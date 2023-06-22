e3636b6d9bf651bcc89faa0195f94a9d1a4c480b
Ardinenukuri <a.nukuri@alustudent.com>
2023-06-22 14:27:42 +0200

    updating

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-#Set up the webServers for the web_static devn't
+# setup nginx server and create a route "/hbnb_static/ to server index.html"
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/we_static/current
-sido chown -hR ubuntu:ubuntu /data/
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<html><head><title>text example</title></head><body>hello world<h1></h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
