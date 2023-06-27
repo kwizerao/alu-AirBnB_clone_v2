d9189fa9783305af541759bf13a562eb5f410835
Ardinenukuri <a.nukuri@alustudent.com>
2023-06-27 14:21:15 +0200

    web_ flask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,46 +1,38 @@
 #!/usr/bin/python3
-"""simple flask routes"""
-from flask import Flask, abort, render_template, render_template_string
+
+"""Starts a Flask web application"""
+
 from models import storage
 from models.state import State
-from models.city import City
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """return message with paramns"""
-    states = storage.all(State).values()
-    sorted_list = sorted(states, key=lambda x: x['name'])
-    return render_template('9-states.html',
-                           states=sorted_list)
+def state_list():
+    """Comment"""
+    states = storage.all('State').values()
+    return render_template(
+        "9-states.html",
+        states=states,
+        condition="states_list")
 
 
-@app.route('/states/<state_id>', strict_slashes=False)
-def cities(state_id):
-    """return message with paramns"""
-    cities = storage.all(City).values()
-    sorted_list = sorted(cities, key=lambda x: x['name'])
-    filtered_list = [d for d in sorted_list if d['state_id'] == state_id]
-    states = storage.all(State).values()
-    state = [d for d in states if d['id'] == state_id]
-    if len(filtered_list) > 0 or len(state) > 0:
-
-        return render_template('9-cities.html',
-                               state=state[0], cities=filtered_list)
-    else:
-        return render_template_string("""<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-
-        <H1>Not found!</H1>
-
-    </BODY>
-</HTML>""")
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+    """Comment"""
+    all_states = storage.all('State')
+    key = "State.{}".format(id)
+    try:
+        state = all_states[key]
+        return render_template(
+            '9-states.html',
+            state=state,
+            condition="state_id")
+    except:
+        return render_template('9-states.html', condition="not_found")
 
 
 @app.teardown_appcontext
@@ -49,5 +41,5 @@ def teardown(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
